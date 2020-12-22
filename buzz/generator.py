07fe04ce63e9c13c4f488451bf3e7ae29864f185
unknown <nabil.elhani@student.hu.nl>
2020-12-23 00:13:48 +0100

    “youmessage2”

--- buzz/generator.py
+++ buzz/generator.py
@@ -8,11 +8,12 @@ adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
-def sample(l,n=1):
+def sample(l, n = 1):
     result = random.sample(l, n)
     if n == 1:
         return result[0]
     return result
+
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
